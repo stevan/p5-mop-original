04997c0c93c768262a726442645d0b211284ec1e
Shawn M Moore <code@sartak.org>
2012-08-27 16:27:56 +0200

    First mopx module! mopx::instance::tracking

---
+++ t/ext/mopx-instance-tracking/mopx/instance/tracking.pm
@@ -0,0 +1,37 @@
+package mopx::instance::tracking;
+use strict;
+use warnings;
+
+use mop;
+use Set::Object::Weak 'weak_set';
+
+class InstanceTracking (extends => $::Class) {
+    has $instances = weak_set();
+
+    method instances { return $instances->members }
+
+    # ->subclasses doesn't work yet
+    # method get_all_instances { return map { $_->instances } $self, $self->subclasses }
+
+    method _track_instance ($instance) {
+        $instances->insert($instance);
+    }
+
+    method _untrack_instance ($instance) {
+        $instances->remove($instance);
+    }
+
+    method create_instance ($params) {
+        my $instance = super($params);
+        $self->_track_instance($instance);
+        return $instance;
+    }
+}
+
+sub import {
+    my $caller = caller;
+    mop->import(-metaclass => InstanceTracking);
+}
+
+1;
+
