c5c0457ba70b7c28de0992a698f1e2b6874d7be5
Stevan Little <stevan.little@iinteractive.com>
2012-09-12 22:21:51 -0400

    even better

--- extras/example/lib/Blog/Model.pm
+++ extras/example/lib/Blog/Model.pm
@@ -1,73 +1,71 @@
-package Blog::Model {
-    use v5.16;
-    use mop;
+package Blog::Model;
+use v5.16;
+use mop;
 
-    role Packable {
-        method pack;
-        method unpack;
+role Packable {
+    method pack;
+    method unpack;
+}
+
+class Author ( roles => [ Packable ] ) {
+    has $name;
+
+    method name { $name }
+
+    method pack { +{ name => $name } }
+
+    method unpack ( $data ) {
+        $name = $data->{'name'};
+        $self;
     }
+}
 
-    class Author ( roles => [ Packable ] ) {
-        has $name;
+class Post ( roles => [ Packable ] )  {
+    has $title;
+    has $author;
+    has $url;
+    has $body;
 
-        method name { $name }
+    method title  { $title }
+    method author { $author }
+    method url    { $url }
+    method body   { $body }
 
-        method pack { +{ name => $name } }
-
-        method unpack ( $data ) {
-            $name = $data->{'name'};
-            $self;
-        }
-    }
-
-    class Post ( roles => [ Packable ] )  {
-        has $title;
-        has $author;
-        has $url;
-        has $body;
-
-        method title  { $title }
-        method author { $author }
-        method url    { $url }
-        method body   { $body }
-
-        method pack {
-            +{
-                title  => $title,
-                author => $author->pack,
-                url    => $url,
-                body   => $body
-            }
-        }
-
-        method unpack ( $data ) {
-            $title  = $data->{'title'};
-            $author = Author->new->unpack( $data->{'author'} );
-            $url    = $data->{'url'};
-            $body   = $data->{'body'};
-            $self;
+    method pack {
+        +{
+            title  => $title,
+            author => $author->pack,
+            url    => $url,
+            body   => $body
         }
     }
 
-    class Blog ( roles => [ Packable ] )  {
-        has @posts;
+    method unpack ( $data ) {
+        $title  = $data->{'title'};
+        $author = Author->new->unpack( $data->{'author'} );
+        $url    = $data->{'url'};
+        $body   = $data->{'body'};
+        $self;
+    }
+}
 
-        method add_post ( $post ) {
-            push @posts => $post
-        }
+class Blog ( roles => [ Packable ] )  {
+    has @posts;
 
-        method pack {
-            +{ posts => [ map { $_->pack } @posts ] }
-        }
-
-        method unpack ( $data ) {
-            foreach my $post ( @{ $data->{'posts'} } ) {
-                push @posts => Post->new->unpack( $post );
-            }
-            $self;
-        }
+    method add_post ( $post ) {
+        push @posts => $post
     }
 
+    method pack {
+        +{ posts => [ map { $_->pack } @posts ] }
+    }
+
+    method unpack ( $data ) {
+        foreach my $post ( @{ $data->{'posts'} } ) {
+            push @posts => Post->new->unpack( $post );
+        }
+        $self;
+    }
 }
 
 1;
--- extras/example/lib/Blog/Model/Util.pm
+++ extras/example/lib/Blog/Model/Util.pm
@@ -1,22 +1,21 @@
-package Blog::Model::Util {
-    use v5.16;
-    use Fun;
-    use JSON::XS ();
+package Blog::Model::Util;
+use v5.16;
 
-    use Blog::Model;
+use Fun;
+use JSON::XS ();
+use Blog::Model;
 
-    use Sub::Exporter -setup => {
-        exports => [qw[ encode_model decode_model ]],
-    };
+use Sub::Exporter -setup => {
+    exports => [qw[ encode_model decode_model ]],
+};
 
-    # NOTE:
-    # hack until doy fixes the Closure
-    # prototype issue with Fun
-    # - SL
-    sub JSON { state $JSON = JSON::XS->new->pretty; $JSON; }
+# NOTE:
+# hack until doy fixes the Closure
+# prototype issue with Fun
+# - SL
+sub JSON { state $JSON = JSON::XS->new->pretty; $JSON; }
 
-    fun encode_model ( $blog ) { JSON->encode( $blog->pack ) }
-    fun decode_model ( $json ) { Blog::Model::Blog->new->unpack( JSON->decode( $json ) ) }
-}
+fun encode_model ( $blog ) { JSON->encode( $blog->pack ) }
+fun decode_model ( $json ) { Blog::Model::Blog->new->unpack( JSON->decode( $json ) ) }
 
 1;
