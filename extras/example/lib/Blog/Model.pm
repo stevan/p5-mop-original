7bdd8c97c3e7aa83be40cbb7bd4dd62888e105bb
Stevan Little <stevan.little@iinteractive.com>
2012-09-23 15:45:16 -0400

    editing and creating posts with an editor

--- extras/example/lib/Blog/Model.pm
+++ extras/example/lib/Blog/Model.pm
@@ -1,17 +1,25 @@
 use v5.16;
 use mop;
 
+use Fun;
 use JSON::XS ();
-
 use Blog::Model::Schema;
 
 my $serializer = JSON::XS->new->pretty;
 
 class Blog::Model {
     has $blog;
+
+    has $editor;
     has $storage;
 
     method add_new_post ( $author, $title, $url, $body ) {
+
+        $author //= $editor->prompt('Author : ');
+        $title  //= $editor->prompt('Title  : ');
+        $url    //= $editor->prompt('URL    : ');
+        $body   //= $editor->invoke;
+
         $blog->add_post(
             Blog::Model::Schema::Post->new(
                 author => Blog::Model::Schema::Author->new( name => $author ),
@@ -20,6 +28,16 @@ class Blog::Model {
                 body   => $body
             )
         );
+    }
+
+    method edit_post ( $url ) {
+        my $post = $blog->find_post( fun ( $post ) { $post->url eq $url } );
+        die "Could not find post for $url" unless $post;
+
+        $post->author( Blog::Model::Schema::Author->new( name => $editor->prompt('Author : ', $post->author->name ) ) );
+        $post->title ( $editor->prompt('Title  : ', $post->title  ) );
+        $post->url   ( $editor->prompt('URL    : ', $post->url    ) );
+        $post->body  ( $editor->invoke( $post->body ) );
     }
 
     method txn_do ( $method, @args ) {
