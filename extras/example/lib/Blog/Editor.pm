335d3dc9da9ab57348a6e12cefe88ff651409993
Stevan Little <stevan.little@iinteractive.com>
2012-09-23 18:03:42 -0400

    switch to Proc::InvokeEditor

--- extras/example/lib/Blog/Editor.pm
+++ extras/example/lib/Blog/Editor.pm
@@ -1,30 +1,16 @@
 use v5.16;
 use mop;
 
-use Path::Class      qw[ file ];
-use File::Temp       qw[ tempfile ];
+use Proc::InvokeEditor;
 use IO::Prompt::Tiny qw[ prompt ];
 
 class Blog::Editor {
-    has $command;
+    has $editors;
 
     method prompt ( $message, $default ) { prompt( $message, $default ) }
 
     method invoke ( $text ) {
-        my ($fh, $filename) = tempfile;
-        if ( $text ) {
-            print $fh $text;
-        }
-        my $status = system( (ref $command ? @$command : $command), $filename );
-        unless ( $status == 0 ) {
-            # Stolen from Proc::InvokeEditor
-            my ($exit_value, $signal_num, $dumped_core);
-            $exit_value  = $? >> 8;
-            $signal_num  = $? & 127;
-            $dumped_core = $? & 128;
-            die "Error in editor invocation: exit val = $exit_value, signal = $signal_num, coredump? = $dumped_core : $!";
-        }
-        return file( $filename )->slurp;
+        Proc::InvokeEditor->new( editors => $editors )->edit( $text // () );
     }
 }
 
